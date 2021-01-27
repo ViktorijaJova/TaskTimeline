c281a7cdb2db4223f5780d8a79a03fba12bb6ad2
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2021-01-27 14:40:37 +0100

    //

---
+++ TaskTimeline/Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button reset;
+    public Button first;
+    public Button second;
+
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject maincamera;
+
+
+    private void Start()
+    {
+
+        maincamera.SetActive(true);
+        camera1.SetActive(false);
+        camera2.SetActive(false);
+        reset.onClick.AddListener(() => Restart());
+        first.onClick.AddListener(() => FirstButton());
+        second.onClick.AddListener(() => SecondButton());
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    void FirstButton()
+    {
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+        maincamera.SetActive(false);
+    }
+    void SecondButton()
+    {
+        maincamera.SetActive(false);
+        camera1.SetActive(false);
+        camera2.SetActive(true);
+    }
+}
+
+
+
---
+++ TaskTimeline/Assets/SecondGameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SecondGameManager : MonoBehaviour
+{
+
+    public GameObject maincamera;
+    public GameObject camera;
+    public Button reset;
+    public Button first;
+    public Button second;
+    void Start()
+    {
+        maincamera.SetActive(true);
+        camera.SetActive(false);
+        reset.onClick.AddListener(() => Restart());
+        first.onClick.AddListener(() => FirstButton());
+        second.onClick.AddListener(() => SecondButton());
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    void FirstButton()
+    {
+
+        camera.SetActive(true);
+           
+    }
+    void SecondButton()
+    {
+        camera.SetActive(true);
+      
+    }
+}
+
+
+    
+
